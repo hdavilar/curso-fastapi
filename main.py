e443db4657647487a150c9095c7be62763f789b0
HECTOR DAVILA RODRIGUEZ <6002990@viewnext.com>
2025-01-29 14:06:32 +0100

    Despliegue

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
-from fastapi.encoders import jsonable_encoder
 from routers.movie import routerMovie
 from routers.users import login_user
+import uvicorn
+import os
 
 app = FastAPI(
     title="Aprendiendo FastAPI",
@@ -20,3 +21,7 @@ app.include_router(login_user)
 def read_root():
     return HTMLResponse("<h2>Hola mundo</h2>")
 
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
